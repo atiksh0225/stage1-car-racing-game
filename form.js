6a5a9d5eb650b3cdbb4b78a98bd38b27fc50ec85
atiksh0225 <66507290+atiksh0225@users.noreply.github.com>
2020-08-08 11:00:51 +0530

    Add files via upload

---
+++ form.js
@@ -0,0 +1,16 @@
+class Form{
+constructor() {
+
+}
+display() {
+var title = createElement("h1")
+title.html("MUTIPLAYER CAR RACING GAME")
+tite.position(150, 10)
+var input = createInput("name")
+var button = createButton("play")
+var greeting = createElement("h3")
+input.position(200, 160);
+button.position(300, 200);
+	
+}
+}
